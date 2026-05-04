e45881383344a34bab1caa5c13e0a4adb9f42bc3
Amrah2609 <emrah.kerimov1111@gmail.com>
2026-05-04 22:54:26 +0400

    Fix register user style

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Module used to authenticate users to database."""
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -9,3 +12,20 @@ def _hash_password(password: str) -> bytes:
         password.encode("utf-8"),
         bcrypt.gensalt()
     )
+
+
+class Auth:
+    """Auth class to interact with the authentication database"""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user."""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+            return self._db.add_user(email, hashed_pwd)
+
+        raise ValueError("User {} already exists".format(email))
